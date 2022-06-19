e9855c7878634a74b41ed307e7f6d2c81ebffb30
Maze <encryptedmaze@gmail.com>
2022-06-19 14:24:51 -0400

    Auto stash before checking out "origin/master"

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,12 +1,12 @@
 #include "Window.hpp"
 
 using namespace MazeSim;
-int main () {
-	Window win = Window("Test", 800, 600);
-	
-	while (win.active()) {
-		win.clear();
-		win.draw();
-	}
-	return 0;
+int main() {
+    Window win = Window("Test", 1280, 720);
+
+    while (win.active()) {
+        win.clear();
+        win.draw();
+    }
+    return 0;
 }
--- src/Window.cpp
+++ src/Window.cpp
@@ -3,14 +3,15 @@ void MazeSim::Window::glfwWindowHints() {
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-	// glfwWindowHint(GLFW_SAMPLES, 8);
 	glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GL_TRUE);
 }
-void MazeSim::Window::glfwErrorCallback (int error, const char* description) {
-    fprintf(stderr, "Glfw Error %d: %s\n", error, description);
+
+void MazeSim::Window::glfwErrorCallback(int error, const char *description) {
+	fprintf(stderr, "Glfw Error %d: %s\n", error, description);
 }
+
 void MazeSim::Window::init() {
-    glfwInit();
+	glfwInit();
 	glfwSetErrorCallback(glfwErrorCallback);
 	Window::glfwWindowHints();
 	const char *title = this->title.c_str();
@@ -18,25 +19,32 @@ void MazeSim::Window::init() {
 	glfwMakeContextCurrent(window);
 	gladLoadGL();
 	glEnable(GL_DEBUG_OUTPUT);
-
+	if (true) {
+		// test
+	} else {
+		// test2
+	}
 	glfwSwapInterval(1); // Enable vsync
 
 	IMGUI_CHECKVERSION();
-    ImGui::CreateContext();
-    ImGuiIO& io = ImGui::GetIO(); (void)io;
+	ImGui::CreateContext();
+	ImGuiIO &io = ImGui::GetIO(); (void) io;
 	ImGui::StyleColorsDark();
 	ImGui_ImplGlfw_InitForOpenGL(window, true);
-	const char* glsl_version = "#version 130";
-    ImGui_ImplOpenGL3_Init(glsl_version);
+	const char *glsl_version = "#version 130";
+	ImGui_ImplOpenGL3_Init(glsl_version);
 
 	this->showDemo = true;
-} 
+}
+
 bool MazeSim::Window::active() {
 	return !glfwWindowShouldClose(this->window);
 }
+
 void MazeSim::Window::clear() {
 
 }
+
 void MazeSim::Window::draw() {
 	this->time = glfwGetTime();
 	this->dt = this->time - this->startTime;
@@ -50,18 +58,19 @@ void MazeSim::Window::draw() {
 	ImGui::NewFrame();
 
 	if (this->showDemo)
-            ImGui::ShowDemoWindow(&this->showDemo);
+		ImGui::ShowDemoWindow(&this->showDemo);
 
 	ImGui::Render();
 
-	int display_w, display_h;
-	glfwGetFramebufferSize(window, &display_w, &display_h);
-	glViewport(0, 0, display_w, display_h);
+	// int display_w, display_h;
+	// glfwGetFramebufferSize(window, &display_w, &display_h);
+	// glViewport(0, 0, display_w, display_h);
 	glClearColor(0.45f, 0.55f, 0.60f, 1.00f);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 	glfwSwapBuffers(this->window);
 }
+
 MazeSim::Window::Window(std::string title, int width, int height) {
 	std::cout << "Window constructor called." << std::endl;
 	this->title = title;
@@ -72,11 +81,12 @@ MazeSim::Window::Window(std::string title, int width, int height) {
 	this->time = 0.0;
 	this->init();
 }
+
 MazeSim::Window::~Window() {
 	ImGui_ImplOpenGL3_Shutdown();
-    ImGui_ImplGlfw_Shutdown();
-    ImGui::DestroyContext();
+	ImGui_ImplGlfw_Shutdown();
+	ImGui::DestroyContext();
 
-    glfwDestroyWindow(this->window);
-    glfwTerminate();
+	glfwDestroyWindow(this->window);
+	glfwTerminate();
 }
