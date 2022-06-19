eefb8171e32eaa40ad1796bbcb97fe51975a4ce9
Maze <encryptedmaze@gmail.com>
2022-06-19 10:58:25 -0400

    Removed glm and FreeImage

--- shaders/static.glsl
+++
@@ -1,6 +0,0 @@
-#version 330 core
-in highp vec2 v_UV;
-out highp vec4 color;
-void main () {
-	color = vec4(0.5f, 0.5f, 1.0f, 0.5f);
-}
--- shaders/static2.glsl
+++
@@ -1,8 +0,0 @@
-#version 330 core
-in highp vec2 v_UV;
-in highp vec3 v_normal;
-out highp vec4 color;
-void main () {
-	// color = vec4((v_normal + vec3(1.0f)) / 2.0f, 1.0f);
-	color = vec4(v_UV, 1.0f, 1.0f);
-}
