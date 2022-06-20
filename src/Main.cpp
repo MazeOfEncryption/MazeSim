841324f6b662cae50a0a2dfa50f78bbe10be71cb
Maze <encryptedmaze@gmail.com>
2022-06-19 20:28:56 -0400

    move test window to Test.cpp, started to implement wire data storage

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,118 +1,62 @@
-#include "Window.hpp"
+#include "Dep.hpp"
+#include <set>
+#include <iostream>
+#include <random>
 
-using namespace MazeSim;
+using Point = std::pair<int, int>;
 
-void ShowCanvas(bool *p_open) {
-    if (!ImGui::Begin("Example: Custom rendering", p_open)) {
-        ImGui::End();
-        return;
-    }
-    static ImVector<ImVec2> points;
-    static ImVec2 scrolling(0.0f, 0.0f);
-    static float zoom = 1.0;
-    static bool opt_enable_grid = true;
-    static bool opt_enable_context_menu = true;
-    static bool adding_line = false;
-
-    ImGui::Checkbox("Enable grid", &opt_enable_grid);
-    ImGui::Checkbox("Enable context menu", &opt_enable_context_menu);
-    ImGui::Text("Mouse Left: drag to add lines,\nMouse Right: drag to scroll, click for context menu.\nScroll wheel: Up to zoom in, Down to scroll out.");
-
-    // Typically you would use a BeginChild()/EndChild() pair to benefit from a clipping region + own scrolling.
-    // Here we demonstrate that this can be replaced by simple offsetting + custom drawing + PushClipRect/PopClipRect() calls.
-    // To use a child window instead we could use, e.g:
-    //      ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0, 0));      // Disable padding
-    //      ImGui::PushStyleColor(ImGuiCol_ChildBg, IM_COL32(50, 50, 50, 255));  // Set a background color
-    //      ImGui::BeginChild("canvas", ImVec2(0.0f, 0.0f), true, ImGuiWindowFlags_NoMove);
-    //      ImGui::PopStyleColor();
-    //      ImGui::PopStyleVar();
-    //      [...]
-    //      ImGui::EndChild();
-
-    // Using InvisibleButton() as a convenience 1) it will advance the layout cursor and 2) allows us to use IsItemHovered()/IsItemActive()
-    ImVec2 canvas_p0 = ImGui::GetCursorScreenPos();      // ImDrawList API uses screen coordinates!
-    ImVec2 canvas_sz = ImGui::GetContentRegionAvail();   // Resize canvas to what's available
-    if (canvas_sz.x < 50.0f) canvas_sz.x = 50.0f;
-    if (canvas_sz.y < 50.0f) canvas_sz.y = 50.0f;
-    ImVec2 canvas_p1 = ImVec2(canvas_p0.x + canvas_sz.x, canvas_p0.y + canvas_sz.y);
-
-    // Draw border and background color
-    ImGuiIO &io = ImGui::GetIO();
-    ImDrawList *draw_list = ImGui::GetWindowDrawList();
-    draw_list->AddRectFilled(canvas_p0, canvas_p1, IM_COL32(50, 50, 50, 255));
-    draw_list->AddRect(canvas_p0, canvas_p1, IM_COL32(255, 255, 255, 255));
-
-    // This will catch our interactions
-    ImGui::InvisibleButton("canvas", canvas_sz, ImGuiButtonFlags_MouseButtonLeft | ImGuiButtonFlags_MouseButtonRight);
-    const bool is_hovered = ImGui::IsItemHovered(); // Hovered
-    const bool is_active = ImGui::IsItemActive();   // Held
-    const ImVec2 origin(canvas_p0.x + scrolling.x, canvas_p0.y + scrolling.y); // Lock scrolled origin
-    const ImVec2 mouse_pos_in_canvas(io.MousePos.x - origin.x, io.MousePos.y - origin.y);
-
-    // Add first and second point
-    if (is_hovered && !adding_line && ImGui::IsMouseClicked(ImGuiMouseButton_Left)) {
-        points.push_back(mouse_pos_in_canvas);
-        points.push_back(mouse_pos_in_canvas);
-        adding_line = true;
-    }
-    if (adding_line) {
-        points.back() = mouse_pos_in_canvas;
-        if (!ImGui::IsMouseDown(ImGuiMouseButton_Left))
-            adding_line = false;
-    }
-
-    // Pan (we use a zero mouse threshold when there's no context menu)
-    // You may decide to make that threshold dynamic based on whether the mouse is hovering something etc.
-    const float mouse_threshold_for_pan = opt_enable_context_menu ? -1.0f : 0.0f;
-    if (is_active && ImGui::IsMouseDragging(ImGuiMouseButton_Right, mouse_threshold_for_pan)) {
-        scrolling.x += io.MouseDelta.x;
-        scrolling.y += io.MouseDelta.y;
-    }
-
-    // Context menu (under default mouse threshold)
-    ImVec2 drag_delta = ImGui::GetMouseDragDelta(ImGuiMouseButton_Right);
-    if (opt_enable_context_menu && drag_delta.x == 0.0f && drag_delta.y == 0.0f)
-        ImGui::OpenPopupOnItemClick("context", ImGuiPopupFlags_MouseButtonRight);
-    if (ImGui::BeginPopup("context")) {
-        if (adding_line)
-            points.resize(points.size() - 2);
-        adding_line = false;
-        if (ImGui::MenuItem("Remove one", NULL, false, points.Size > 0)) { points.resize(points.size() - 2); }
-        if (ImGui::MenuItem("Remove all", NULL, false, points.Size > 0)) { points.clear(); }
-        ImGui::EndPopup();
-    }
-
-    // Draw grid + all lines in the canvas
-    draw_list->PushClipRect(canvas_p0, canvas_p1, true);
-    if (opt_enable_grid) {
-        const float GRID_STEP = 64.0f;
-        for (float x = fmodf(scrolling.x, GRID_STEP); x < canvas_sz.x; x += GRID_STEP)
-            draw_list->AddLine(ImVec2(canvas_p0.x + x, canvas_p0.y), ImVec2(canvas_p0.x + x, canvas_p1.y), IM_COL32(200, 200, 200, 40));
-        for (float y = fmodf(scrolling.y, GRID_STEP); y < canvas_sz.y; y += GRID_STEP)
-            draw_list->AddLine(ImVec2(canvas_p0.x, canvas_p0.y + y), ImVec2(canvas_p1.x, canvas_p0.y + y), IM_COL32(200, 200, 200, 40));
-    }
-    for (int n = 0; n < points.Size; n += 2)
-        draw_list->AddLine(ImVec2(origin.x + points[n].x, origin.y + points[n].y), ImVec2(origin.x + points[n + 1].x, origin.y + points[n + 1].y), IM_COL32(255, 255, 0, 255), 2.0f);
-    draw_list->PopClipRect();
-    ImGui::End();
+Point toRectangular (Point in) {
+    return {in.second % 2 == 0 ? in.first * 2 : in.first * 2 + 1, in.second};
 }
-bool showCanvas = false;
-int main() {
-    Window win = Window("Test", 1280, 720);
-    bool showDemo = false;
-    while (win.active()) {
-        win.clear();
-        if (ImGui::BeginMainMenuBar()) {
-            if (ImGui::BeginMenu("Help")) {
-                ImGui::MenuItem("Show Demo Window", NULL, &showDemo);
-                ImGui::MenuItem("Show Canvas Window", NULL, &showCanvas);
-                ImGui::EndMenu();
-            }
-            ImGui::EndMainMenuBar();
-        }
-        if (showDemo) ImGui::ShowDemoWindow(&showDemo);
-        if (showCanvas) ShowCanvas(&showCanvas);
-        win.draw();
+
+int main () {
+    std::set<Point> board;
+
+    // board.insert({ 2, -1});
+    // board.insert({ 1,  2});
+    // board.insert({-4,  3});
+    board.insert({ -2, -2});
+    board.insert({ -2, -1});
+    board.insert({ -2,  0});
+    board.insert({ -2,  1});
+    board.insert({ -2,  2});
+    board.insert({ -1, -2});
+    board.insert({ -1, -1});
+    board.insert({ -1,  0});
+    board.insert({ -1,  1});
+    board.insert({ -1,  2});
+    board.insert({  0, -2});
+    board.insert({  0, -1});
+    board.insert({  0,  0});
+    board.insert({  0,  1});
+    board.insert({  0,  2});
+    board.insert({  1, -2});
+    board.insert({  1, -1});
+    board.insert({  1,  0});
+    board.insert({  1,  1});
+    board.insert({  1,  2});
+    board.insert({  2, -2});
+    board.insert({  2, -1});
+    board.insert({  2,  0});
+    board.insert({  2,  1});
+    board.insert({  2,  2});
+
+    // std::mt19937 gen(0);
+    // std::uniform_int_distribution<int> dis(-4, 4);
+
+    // for (int i = 0; i < 10; i++) {
+    //     board.insert({dis(gen), dis(gen)});
+    // }
+
+    for (Point point : board) {
+        Point rect = toRectangular(point);
+        std::cout << "(" << ((point.first<0)?(""):(" ")) << point.first << ", ";
+        std::cout << ((point.second<0)?(""):(" ")) << point.second << ")";
+        std::cout << " -> ";
+        std::cout << "(" << ((rect.first<0)?(""):(" ")) << rect.first << ", ";
+        std::cout << ((rect.second<0)?(""):(" ")) << rect.second << ")";
+        std::cout << std::endl;
     }
+
     return 0;
 }
